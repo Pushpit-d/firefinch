64ce2fa8d15704967bc2c69dc823a9eb8703bd58
pushpitf9 <developer@f9tech.com>
2025-12-07 02:26:53 +0530

    phone number change

--- src/components/HomeThree/FooterHomeOne.jsx
+++ src/components/HomeThree/FooterHomeOne.jsx
@@ -96,7 +96,7 @@ function FooterHomeOne({ className }) {
                     </li>
                     <li>
                       <i className="fa fa-phone-square"></i>{" "}
-                      <Link to="tel:+97142621654">+971 58 594 0567</Link>
+                      <Link to="tel:+97142621654">+971 4 262 1654</Link>
                     </li>
                   </ul>
                 </div>
